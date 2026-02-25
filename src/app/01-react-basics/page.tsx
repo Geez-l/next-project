3ab7c916afada0e9a3a049d990199d896568625a
Gliezel Ann Pajarilla <gtpajarilla@up.edu.ph>
2026-02-26 01:38:07 +0800

    Added complexity and debugging

--- src/app/01-react-basics/page.tsx
+++ src/app/01-react-basics/page.tsx
@@ -1,13 +1,22 @@
-
+import "@/app/css/01-react-basics-css/01.css";
 
 export default function ReactBasics() {
   return (
-    <main>
-      <h1 className="text-title text-center py-xl-4 text-muted">This section introduces the basics of React. </h1>
+    <main className="container">
+      <div className="header-title text-center py-xl-4 text-muted">
+        <h1>React Basics </h1>
+        <p>This section introduces the basics of React.</p>
+      </div>
 
-      <section className="materials-section">
-        
-      </section>
+      <div className="material-section">
+        <section className="section">
+          <h2 className="py-1 align-content-start">1. Cards </h2>
+          <div className="highlight py-md-4">
+            <strong>Definition:</strong> a common UI component used to group
+            related information into a single, visually distinct container.{" "}
+          </div>
+        </section>
+      </div>
     </main>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,44 @@
+"use client"
 import HomeCards from "./components/cards";
 import "./css/cards.css";
 
+import { useEffect, useState } from "react";
+
 export default function Home() {
+
+  const fullText = " This is a compilation of things to learn and explore in the world of web development, covering a wide range of topics from React basics to advanced concepts."
+  const typingSpeed=50;
+  const deletingSpeed=30;
+  const pauseTime=1200;
+
+  
+  const [displayText, setDisplayedText] = useState("");
+  const [isDeleting, setDeleting] = useState(false)
+  const [index, setIndex] = useState(0);
+
+  useEffect(() => {
+    let timeout;
+
+    if (!isDeleting && index < text.length) {
+      timeout = setTimeout()
+    }
+    if (index < fullText.length){
+      const timeout = setTimeout(() =>{
+        // setDisplayedText((prev) => prev + fullText[index]);
+        setIndex((prev) => prev + 1);
+      }, 30);
+
+      return () => clearTimeout(timeout);
+    }
+  }, [index, fullText]);
   return (
         <div className="container-fluid align-content-center py-4">
           <h2 className="text-center fs-1" style={{ fontSize: "2.5rem" }}>
             Welcome
           </h2>
-          <p className="text-muted text-center" style={{ fontSize: "1.2rem" }}>
-            This is a compilation of things to learn and explore in the world of
-            web development, covering a wide range of topics from React basics
-            to advanced concepts.
+          <p className="home-description text-center text-muted fs-5">
+            {displayText}
+            <span className="cursor"></span>
           </p>
           <div>
             <HomeCards />
