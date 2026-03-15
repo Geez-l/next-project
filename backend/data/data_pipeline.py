9ca5d44021c89165c0571ec2b19e46ff0020d7aa
Gliezel Ann Pajarilla <gtpajarilla@up.edu.ph>
2026-03-15 22:19:33 +0800

    updated the pipeline: loading csv and cleaning

--- backend/data/data_pipeline.py
+++ backend/data/data_pipeline.py
@@ -2,7 +2,9 @@
 
 import psycopg2
 from config import config
+import csv
 
+csv_file = "/backend/data/penguins.csv"
 # create connection
 def connect():
     conn = None
@@ -27,6 +29,50 @@ def connect():
         if conn is not None:
             conn.close()
             print("Database connection terminated.")
+
+def ingest_data():
+    # connect to postgresql
+    conn = connect()
+    curr = conn.cursor()
+    print("Loading csv to the staging table")
+
+    # open the csv file
+    with open(csv_file, 'r', encoding="utf-8") as file:
+        curr.copy_expert(
+            """
+        COPY penguins_staging FROM STDIN
+        DELIMITER as ','
+            """,
+            file
+        )
+
+        conn.commit()
+        curr.close()
+        conn.close()
+        print("Data ingested successfully")
+
+# clean the data
+def clean_data():
+    conn = connect()
+    curr = conn.cursor()
+    print("Cleaning data")
+
+    # remove rows without species
+    curr.execute("""
+    DELETE FROM penguins_staging 
+    WHERE species is NULL
+    """)
+
+    # standardize species name
+    curr.execute("""
+    UPDATE penguins_staging 
+    SET species = INITCAP(species)
+    """)
+
+    conn.commit()
+    curr.close()
+    conn.close()
+    print("Data cleaned")
 
 if __name__ == "__main__":
     connect()
